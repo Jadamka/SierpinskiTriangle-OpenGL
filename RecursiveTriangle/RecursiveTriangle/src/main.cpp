728a342b35b9250e822ac31dca7ebe50bb3ffd32
Jadamka <59932297+Jadamka@users.noreply.github.com>
2024-08-01 22:45:27 +0200

    Finished project

--- RecursiveTriangle/RecursiveTriangle/src/main.cpp
+++ RecursiveTriangle/RecursiveTriangle/src/main.cpp
@@ -1,9 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <iostream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 #include <random>
 #include "../include/shader.h"
 
